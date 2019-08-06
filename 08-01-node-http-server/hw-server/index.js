0b69c023e24df9afa0d7aae410006b0eb34c784d
elencode <40804888+elencode@users.noreply.github.com>
2019-08-06 14:49:44 +0300

    Add files via upload

---
+++ 08-01-node-http-server/hw-server/index.js
@@ -0,0 +1,20 @@
+let http = require("http")
+let fs = require("fs")
+
+let server = http.createServer( (req,res) => {
+
+    console.log( req.url )
+    if (req.url == "/") {
+        var html = fs.readFileSync("./html/home.html")
+    } else if (req.url.endsWith("css")) {
+        var html = fs.readFileSync("./css/*.css")
+    } else if (req.url == "/images/icon.png") {
+        var html = fs.readFileSync("./images/icon.png")
+    } else {
+        var html = "not found!"
+    }
+    
+    res.write(html)
+    res.end()
+})
+server.listen(3000)
