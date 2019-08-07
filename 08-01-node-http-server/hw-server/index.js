7bf59ac9829635f9e8a1448a6a8ae6393c019fbe
elencode <rusuelen@gmail.com>
2019-08-07 11:15:42 +0300

    add

--- 08-01-node-http-server/hw-server/index.js
+++ 08-01-node-http-server/hw-server/index.js
@@ -6,8 +6,8 @@ let server = http.createServer( (req,res) => {
     console.log( req.url )
     if (req.url == "/") {
         var html = fs.readFileSync("./html/home.html")
-    } else if (req.url.endsWith("css")) {
-        var html = fs.readFileSync("./css/*.css")
+    } else if (req.url.endsWith(".css")) {
+        var html = fs.readFileSync("." + req.url )
     } else if (req.url == "/images/icon.png") {
         var html = fs.readFileSync("./images/icon.png")
     } else {
